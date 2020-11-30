f94354a616f1a887f83286da9b5c0d139c43667d
WNI24 <617520834@qq.com>
2020-11-30 14:17:12 +0800

    bl

---
+++ src/main/java/com/dto/MetaDto.java
@@ -0,0 +1,20 @@
+
+ package com.dto;
+
+import com.model.MetaDomain;
+
+/**
+ * 标签、分类列表
+ */
+public class MetaDto extends MetaDomain {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ src/main/java/com/service/user/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.service.user;
+
+import com.dao.UserDao;
+import com.exception.BusinessException;
+import com.exception.ErrorConstant;
+import com.model.UserDomain;
+import com.utils.TaleUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 用户相关Service接口实现
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+
+    @Override
+    public UserDomain login(String username, String password) {
+    
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password))
+            throw BusinessException.withErrorCode(ErrorConstant.Auth.USERNAME_PASSWORD_IS_EMPTY);
+    
+        String pwd = TaleUtils.MD5encode(username + password);
+        UserDomain user = userDao.getUserInfoByCond(username,pwd);
+        if (null == user)
+            throw BusinessException.withErrorCode(ErrorConstant.Auth.USERNAME_PASSWORD_ERROR);
+        return user;
+    }
+    
+    @Override
+    public UserDomain getUserInfoById(Integer uid) {
+        return userDao.getUserInfoById(uid);
+    }
+    
+    // 开启事务
+    @Transactional
+    @Override
+    public int updateUserInfo(UserDomain user) {
+        if (null == user.getUid())
+            throw BusinessException.withErrorCode("用户编号不能为空");
+        return userDao.updateUserInfo(user);
+    }
+}
